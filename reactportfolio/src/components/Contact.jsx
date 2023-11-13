7eb7202802dea5199aff31cb9084d9032fab475b
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-11-12 22:47:37 -0600

    finished project

--- reactportfolio/src/components/Contact.jsx
+++ reactportfolio/src/components/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../colors.css"
 
-function Contact() {
+// switched to using a google form, easier for me to track responses + safer
+const Contact = () => {
+  return (
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-half">
+          {/* google forms has an iframe embed you can use, pretty easy to implement */}
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfwfAPB1InalnZg-NUJ4UteTSLU0SmBU4_hAADWyidTmxtClA/viewform?embedded=true" width="640" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+      </div>
+
+      </div>
+  );
+};
   
-}
-
-export default Contact; 
+  export default Contact;
